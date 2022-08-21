c1efef5dc504f67e7e46166463b0881352797f92
阿提说说 <0431cjj@163.com>
2022-08-22 00:50:22 +0800

    shenyu demo

---
+++ server1/src/main/java/com/shenyu/demo/server2/Server1Application.java
@@ -0,0 +1,13 @@
+package com.shenyu.demo.server2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Server1Application.class, args);
+    }
+
+}
