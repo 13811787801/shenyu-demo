201eed599a1c33c32f2537b24f2e61806eb26180
阿提说说 <0431cjj@163.com>
2022-08-26 01:34:18 +0800

    测试

--- server1/src/main/java/com/shenyu/demo/server1/HelloController.java
+++ server1/src/main/java/com/shenyu/demo/server1/HelloController.java
@@ -1,14 +1,22 @@
 package com.shenyu.demo.server1;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
 @RestController
 public class HelloController {
 
     @PostMapping("/hello")
-    public String hello() {
+    public String hello(HttpServletResponse response) throws UnknownHostException {
+        log.info("ip:{}", InetAddress.getLocalHost().getHostAddress());
         return "Server1 Hello ShenYu";
     }
 }
--- server1/src/main/java/com/shenyu/demo/server1/Server1Application.java
+++ server1/src/main/java/com/shenyu/demo/server1/Server1Application.java
@@ -2,7 +2,9 @@ package com.shenyu.demo.server1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Server1Application {
 
